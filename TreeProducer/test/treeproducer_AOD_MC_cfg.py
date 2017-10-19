7d514909b0d14591b2f2f1cc0d1ce5a4839b5b99
gflouris <flourisj@gmail.com>
2017-10-19 13:13:31 +0200

    report every 500

--- TreeProducer/test/treeproducer_AOD_MC_cfg.py
+++ TreeProducer/test/treeproducer_AOD_MC_cfg.py
@@ -10,11 +10,13 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_TrancheIV_v8', '')
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(500)
+
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-     'file:///user/gflouris/Analysis/SIMPS/SignalProduction/testPG/CMSSW_8_0_21/src/SUS-RunIISummer16DR80Premix-00068.root'
+     'file:///user/gflouris/Analysis/SIMPS/SignalProduction/testPG/CMSSW_8_0_21/src/NoPile/SUS-RunIISummer16DR80Premix-00068.root'
     )
 )
 
