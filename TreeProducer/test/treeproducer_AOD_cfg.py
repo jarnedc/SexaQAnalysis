4aa1b5277c407e16ae86b848cc68c0e43abe4ac4
gflouris <flourisj@gmail.com>
2017-10-25 15:41:52 +0200

    update GT

--- TreeProducer/test/treeproducer_AOD_cfg.py
+++ TreeProducer/test/treeproducer_AOD_cfg.py
@@ -27,9 +27,9 @@ process.load('Configuration/EventContent/EventContent_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
 
 if(options.isData==True):
-    process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_TrancheIV_v8', '')
+    process.GlobalTag = GlobalTag(process.GlobalTag, '92X_dataRun2_2017Repro_v4', '')
 else:
-    process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+    process.GlobalTag = GlobalTag(process.GlobalTag, '92X_upgrade2017_realistic_v7', '')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvts))
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(500)
@@ -37,7 +37,10 @@ process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(500)
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-     'file:///user/gflouris/Analysis/SIMPS/SignalProduction/testPG/CMSSW_8_0_21/src/NoPile/SUS-RunIISummer16DR80Premix-00068.root'
+     #'file://./E4C41A5A-1976-E711-B680-02163E01439D.root'
+     #'file:///user/gflouris/Analysis/SIMPS/SignalProduction/testPG/CMSSW_8_0_21/src/NoPile/SUS-RunIISummer16DR80Premix-00068.root'
+     'file://./0679350A-05A3-E711-A1F8-0025905B85CA.root'
+
     )
 )
 
