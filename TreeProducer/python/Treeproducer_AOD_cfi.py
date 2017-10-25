dcbeaa1108524b1efac7a6b2305bce285b2c1da0
gflouris <flourisj@gmail.com>
2017-10-25 13:19:42 +0200

    remove unused pars

--- TreeProducer/python/Treeproducer_AOD_cfi.py
+++ TreeProducer/python/Treeproducer_AOD_cfi.py
@@ -3,9 +3,6 @@ import FWCore.ParameterSet.Config as cms
 tree = cms.EDAnalyzer(
     'TreeProducer_AOD',
     triggerResults   = cms.InputTag("TriggerResults", "", "HLT"),
-    prescales        = cms.InputTag("patTrigger", "", "RECO"), #not found
-    prescales2        = cms.InputTag("patTrigger", "", "PAT"), #not found
-    genjetCollection  = cms.InputTag("ak4GenJetsNoNu", "", "HLT"),
     vertexCollection = cms.InputTag("offlinePrimaryVertices"),
     trackCollection    = cms.InputTag("generalTracks", "", "RECO"),
     isData = cms.untracked.bool(True),
