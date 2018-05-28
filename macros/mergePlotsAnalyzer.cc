5cf5800c779a8074a26552fd9dd09d3ab7cd15f8
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2018-05-28 12:40:23 +0200

    modifying script to overlay histograms

--- macros/mergePlotsAnalyzer.cc
+++ macros/mergePlotsAnalyzer.cc
@@ -44,35 +44,25 @@ void mergePlotsAnalyzer()
    //Author: Rene Brun
 
    TFile *OutputFile = new TFile("Combine_Analyzer_plots.root","RECREATE");
-   TFile *hfile1 = new TFile("../MET.root");
-   TFile *hfile2 = new TFile("../ZeroBias.root"); 
+   
+   vector<string> data;
+   data.push_back("MET");
+   data.push_back("SingleMuon");
+   
+   TFile *hfile1 = new TFile(("../Data_analysis/Results/"+data[0]+".root").c_str());
+   TFile *hfile2 = new TFile(("../Data_analysis/Results/"+data[1]+".root").c_str());
 	
-   vector<string> v_histo;
-   v_histo.push_back("analyzer/WjetsMC_rCandMass_L0");
-   v_histo.push_back("analyzer/WjetsMC_rCandMass_L0_with_dxy(sCandVtx_bs)_over_2cm_cut_and_conditions_on_errors");
-   v_histo.push_back("analyzer/WjetsMC_sCandMass_L0");
-   v_histo.push_back("analyzer/WjetsMC_rCandMass_antiL0");
-   v_histo.push_back("analyzer/WjetsMC_rCandMass_antiL0_with_dxy(sCandVtx_bs)_over_2cm_cut_and_conditions_on_errors");
-   v_histo.push_back("analyzer/WjetsMC_rCandMass_with_dxy_smaller_0p2_and_dr_daughters_smaller_0p8");
-   v_histo.push_back("analyzer/WjetsMC_sCand_delta_phi");
-   v_histo.push_back("analyzer/WjetsMC_sCand_delta_phi_dz(PCA_PV0)_below_1mm");
-   v_histo.push_back("analyzer/WjetsMC_sCand_delta_eta_dz(PCA_PV0)_above_1mm");
-   v_histo.push_back("analyzer/WjetsMC_sCand_delta_eta");
-   v_histo.push_back("analyzer/WjetsMC_sCand_delta_R");
-   v_histo.push_back("analyzer/WjetsMC_sCand_delta_R_dz(PCA_PV0)_below_1mm");
-   v_histo.push_back("analyzer/WjetsMC_sCand_dxy(sCandVtx_beamspot)_signed_anti_L0");
+   vector<string> v_histos;
+   v_histos.push_back("_sCand_delta_phi");
    
-   
-   for(int i = 0; i < v_histo.size(); i++  ){
-	   TCanvas *c1 = new TCanvas(v_histo[i].c_str(),v_histo[i].c_str(),600,400);
+   for(int i = 0; i < (int)v_histos.size(); i++  ){
+	   TCanvas *c1 = new TCanvas(v_histos[i].c_str(),v_histos[i].c_str(),600,400);
 	   gStyle->SetOptStat(kFALSE);
 	 //  auto legend = new TLegend(0.1,0.7,0.48,0.9);
-	   
-	   TH1F *h1 = hfile1->Get(v_histo[i].c_str()); 
-	   TH1F *h2 = hfile2->Get(v_histo[i].c_str());
-
-           
-
+	   cout << ("analyzer/"+data[0]+v_histos[i]).c_str() << endl;
+	   cout << ("analyzer/"+data[1]+v_histos[i]).c_str() << endl; 
+	   TH1F *h1 = (TH1F*)hfile1->Get(("analyzer/"+data[0]+v_histos[i]).c_str()); 
+	   TH1F *h2 = (TH1F*)hfile2->Get(("analyzer/"+data[1]+v_histos[i]).c_str()); 
 
 	   h1->Scale(1/h1->GetEntries());
 	   h2->Scale(1/h2->GetEntries());
@@ -96,9 +86,14 @@ void mergePlotsAnalyzer()
 	   h1->GetYaxis()->SetRangeUser(0,0.01);
 	   h2->GetYaxis()->SetRangeUser(0,0.01);
 	   c1->Update();
+
+	   auto legend = new TLegend(0.1,0.7,0.48,0.9);
+   	   legend->AddEntry(h1,data[0].c_str(),"l");
+   	   legend->AddEntry(h2,data[1].c_str(),"l");
+   	   legend->Draw();
 	   
-   OutputFile->cd();
-   c1->Write();
+           OutputFile->cd();
+           c1->Write();
 
    }
 
