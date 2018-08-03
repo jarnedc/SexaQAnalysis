8a0c77da34ae79e2b07db3467a597c56e6e3afed
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2018-08-03 20:22:16 +0200

    crab configuration files to run the normal filter flow

---
+++ TreeProducer/test/crab/crabcfg_LowPU.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config
+
+config = config()
+
+pyCfgParams = ['isData=True']
+
+config.General.requestName = 'SexaQ_LowPU'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '../treeproducer_data_cfg.py'
+
+config.Data.inputDataset = '/SingleMuon/Run2016H-07Aug17-v1/AOD'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 1000000
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON_LowLowPU.txt'
+config.Data.runRange = ''
+config.Data.outLFNDirBase = '/store/user/jdeclerc/'
+config.Data.publication = False
+config.Data.outputDatasetTag = 'LowPU_Run2016H'
+
+config.Site.storageSite = 'T2_BE_IIHE'
