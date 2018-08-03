63d2a7e45ee44e8bdf92186b2e7213fe3ad449a5
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2018-08-03 18:54:35 +0200

    changing only_Lambda_Kshort_filter configuration for other keeps

--- V0_angular_correlation/test/treeproducer_V0_correlation_data_onl_LambdaKshortFilter.py
+++ V0_angular_correlation/test/treeproducer_V0_correlation_data_onl_LambdaKshortFilter.py
@@ -18,7 +18,7 @@ options.register(
 process = cms.Process("SEXAQ")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1)
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
@@ -42,8 +42,8 @@ process.source = cms.Source("PoolSource",
    duplicateCheckMode = cms.untracked.string('checkAllFilesOpened'),
    fileNames = cms.untracked.vstring(
     # 'file:///user/lowette/SexaQ/RunIISummer16DR80_MinBias_TuneCUETP8M1_13TeV-pythia8_GEN-SIM-RECO_NoPU_RECO_80X_mcRun2_asymptotic_v14-v1_100000_00150044-D075-E611-AAE8-001E67505A2D.root',
-    # 'file:///user/lowette/SexaQ/Run2016H_SingleMuon_AOD_07Aug17-v1_10000_F068EC7F-FB96-E711-96FF-7845C4FC3BFF.root',
-    'file:///user/jdeclerc/Analysis/dataSamples/ZeroBiasData/D07C27A2-5690-E711-844F-B083FED429D5.root'
+     'file:///user/lowette/SexaQ/Run2016H_SingleMuon_AOD_07Aug17-v1_10000_F068EC7F-FB96-E711-96FF-7845C4FC3BFF.root',
+   # 'file:///user/jdeclerc/Analysis/dataSamples/ZeroBiasData/D07C27A2-5690-E711-844F-B083FED429D5.root'
    #'file:///user/jdeclerc/Analysis/ParticleGun/CMSSW_8_0_21/src/f2prime1525_GENSIM_IIDD_0point5to10GeV1k_MODIFIEDBRANCHINGRATIOS_MODPHI_MODBEAMSPOT_0cm_step2.root'
    )
 )
@@ -51,80 +51,89 @@ process.source = cms.Source("PoolSource",
 
 process.nEvTotal        = cms.EDProducer("EventCountProducer")
 process.nEvLambdaKshort = cms.EDProducer("EventCountProducer")
-#process.nEvLambdaKshortVertex = cms.EDProducer("EventCountProducer")
-#process.nEvSMass        = cms.EDProducer("EventCountProducer")
+process.nEvLambdaKshortVertex = cms.EDProducer("EventCountProducer")
+process.nEvSMass        = cms.EDProducer("EventCountProducer")
 
-#process.genParticlePlusGEANT = cms.EDProducer("GenPlusSimParticleProducer",
-#  src           = cms.InputTag("g4SimHits"),
-#  setStatus     = cms.int32(8),                # set status = 8 for GEANT GPs
-#  particleTypes = cms.vstring("Xi-","Xibar+","Lambda0","Lambdabar0","K_S0","K0"),      # also picks pi- (optional)
-#  filter        = cms.vstring("pt >= 0.0"),     # just for testing
-#  genParticles  = cms.InputTag("genParticles") # original genParticle list
+process.genParticlePlusGEANT = cms.EDProducer("GenPlusSimParticleProducer",
+  src           = cms.InputTag("g4SimHits"),
+  setStatus     = cms.int32(8),                # set status = 8 for GEANT GPs
+  particleTypes = cms.vstring("Xi-","Xibar+","Lambda0","Lambdabar0","K_S0","K0"),      # also picks pi- (optional)
+  filter        = cms.vstring("pt >= 0.0"),     # just for testing
+  genParticles  = cms.InputTag("genParticles") # original genParticle list
+)
+
+#process.InitialProducer = cms.EDProducer('InitialProducer'
+#        ,TrackCollection    =cms.InputTag('generalTracks')
 #)
 
+
 process.load("SexaQAnalysis.Skimming.LambdaKshortFilter_cfi")
-#process.lambdaKshortFilter.genCollection = cms.InputTag("genParticlePlusGEANT")
+process.lambdaKshortFilter.genCollection = cms.InputTag("genParticlePlusGEANT")
 process.lambdaKshortFilter.isData = True
 process.lambdaKshortFilter.minPtLambda = 1.5 # gives pT(proton,pion)>0.5GeV
 process.lambdaKshortFilter.minPtKshort = 0.9 # gives pT(pion,pion)>0.5GeV
 process.lambdaKshortFilter.checkLambdaDaughters = True
 process.lambdaKshortFilter.prescaleFalse = 0
 
-#for this special check of overlaps between angular correlation of strange particles only keep the filter that gets rid of the overlap
+process.load("SexaQAnalysis.Skimming.LambdaKshortVertexFilter_cfi")
+process.lambdaKshortVertexFilter.lambdaCollection = cms.InputTag("lambdaKshortFilter","lambda")
+process.lambdaKshortVertexFilter.kshortCollection = cms.InputTag("lambdaKshortFilter","kshort")
+process.lambdaKshortVertexFilter.maxchi2ndofVertexFit = 10.
 
-#process.load("SexaQAnalysis.Skimming.LambdaKshortVertexFilter_cfi")
-#process.lambdaKshortVertexFilter.lambdaCollection = cms.InputTag("lambdaKshortFilter","lambda")
-#process.lambdaKshortVertexFilter.kshortCollection = cms.InputTag("lambdaKshortFilter","kshort")
-#process.lambdaKshortVertexFilter.maxchi2ndofVertexFit = 10.
+from SexaQAnalysis.Skimming.MassFilter_cfi import massFilter
+massFilter.lambdakshortCollection = cms.InputTag("lambdaKshortVertexFilter","sParticles")
+massFilter.minMass = -10000 # effectively no filter
+massFilter.maxMass = 10000  # effectively no filter
+process.rMassFilter = massFilter.clone()
+process.rMassFilter.targetMass = 0
+process.sMassFilter = massFilter.clone()
+process.sMassFilter.targetMass = 0.939565
 
-#from SexaQAnalysis.Skimming.MassFilter_cfi import massFilter
-#massFilter.lambdakshortCollection = cms.InputTag("lambdaKshortVertexFilter","sParticles")
-#massFilter.minMass = -10000 # effectively no filter
-#massFilter.maxMass = 10000  # effectively no filter
-#process.rMassFilter = massFilter.clone()
-#process.rMassFilter.targetMass = 0
-#process.sMassFilter = massFilter.clone()
-#process.sMassFilter.targetMass = 0.939565
+process.load("SexaQAnalysis.Skimming.InitialProducer_cff")
 
-
-process.load("SexaQAnalysis.TreeProducer.Treeproducer_AOD_cfi")
+#process.load("SexaQAnalysis.TreeProducer.Treeproducer_AOD_cfi")
 #process.tree.genCollection = cms.InputTag("genParticlePlusGEANT")
 #process.tree.sCollection = cms.InputTag("lambdaKshortVertexFilter","sParticles")
 #process.tree.sTrackCollection = cms.InputTag("lambdaKshortVertexFilter","sParticlesTracks")
-process.tree.isData = cms.untracked.bool(options.isData)
+#process.tree.isData = cms.untracked.bool(options.isData)
 
 process.p = cms.Path(
+#  process.genParticlePlusGEANT *
   process.nEvTotal *
   process.lambdaKshortFilter *
   process.nEvLambdaKshort *
-
-#  process.nEvTotal *
-#  process.lambdaKshortFilter *
-#  process.nEvLambdaKshort *
-
- # process.lambdaKshortVertexFilter *
- # process.nEvLambdaKshortVertex *
- # process.rMassFilter *
- # process.sMassFilter *
- # process.nEvSMass *
-  process.tree 
+#  process.lambdaKshortVertexFilter *
+#  process.nEvLambdaKshortVertex *
+#  process.rMassFilter *
+#  process.sMassFilter *
+#  process.nEvSMass *
+  process.InitialProducer 
+#  process.tree 
 )
 
-# Output
-process.TFileService = cms.Service('TFileService',
-    fileName = cms.string('ZeroBias_LambdaKshortFilter_only_single_file.root')
-)
+# Output --> not used in the analyzer
+#process.TFileService = cms.Service('TFileService',
+#    fileName = cms.string('tree.root')
+#)
 
-#Keep edm output file only during debugging
+#Keep edm output file --> used in the analyzer
 process.out = cms.OutputModule("PoolOutputModule",
   outputCommands = cms.untracked.vstring(
-    'drop *',
+     'drop *',
+
+#    'drop *',
+#    'keep *_InitialProducer_*_*',
     'keep recoVertexCompositeCandidates_generalV0Candidates_*_*',
-    'keep recoVertexs_offlinePrimaryVertices_*_*',
-    'keep *_offlineBeamSpot_*_*',
-    'keep *_*_*_SEXAQ',
+    'keep recoTracks_lambdaKshortVertexFilter_sParticlesTracks_*',
+    'keep recoVertexCompositePtrCandidates_rMassFilter_sVertexCompositePtrCandidate_*', 
+    'keep recoVertexCompositePtrCandidates_sMassFilter_sVertexCompositePtrCandidate_*', 
+    'keep *_*_*_SEXAQ'
+
+ #   'keep recoVertexs_offlinePrimaryVertices_*_*',
+ #   'keep *_offlineBeamSpot_*_*',
+ #   'keep *_*_*_SEXAQ',
   ),
-  fileName = cms.untracked.string("V0_correlation_ZeroBias_single_file.root"),
+  fileName = cms.untracked.string("events_skimmed.root"),
   SelectEvents = cms.untracked.PSet(
     SelectEvents = cms.vstring('p')
   )
