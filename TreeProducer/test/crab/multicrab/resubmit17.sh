fa607e710d9f3f0b74bb87b3ee83caeab1b47c74
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2018-10-23 20:03:28 +0200

    for trial D

--- TreeProducer/test/crab/multicrab/resubmit17.sh
+++ TreeProducer/test/crab/multicrab/resubmit17.sh
@@ -7,7 +7,7 @@ cd /user/jdeclerc/Analysis/SexaQuark/CMSSW_9_4_7/src/                          #
 eval `scram runtime -sh`                                         # don't use cmsenv, won't work on batch                                                                                                                                            
 cd $pwd
 
-for D in `find ./crab_projects17_trialC -type d` -maxdepth 0
+for D in `find ./crab_projects17_trialD -type d` -maxdepth 0
 do
     echo WILL RUN ON THE FOLLOWING DIRECTORY
     echo $D
--- TreeProducer/test/treeproducer_data_cfg.py
+++ TreeProducer/test/treeproducer_data_cfg.py
@@ -44,7 +44,7 @@ process.source = cms.Source("PoolSource",
     # 'file:///user/lowette/SexaQ/RunIISummer16DR80_MinBias_TuneCUETP8M1_13TeV-pythia8_GEN-SIM-RECO_NoPU_RECO_80X_mcRun2_asymptotic_v14-v1_100000_00150044-D075-E611-AAE8-001E67505A2D.root',
     # 'file:///user/lowette/SexaQ/Run2016H_SingleMuon_AOD_07Aug17-v1_10000_F068EC7F-FB96-E711-96FF-7845C4FC3BFF.root',
     #Simulated Xi events:
-     'file:///user/jdeclerc/Analysis/ParticleGun/CMSSW_8_0_21/src/XiGun_10k_decay_length_5cm_events_no_EvtGen_Ks_310_GENSIM_IIDD_step2.root',
+     'file:///user/jdeclerc/Analysis/ParticleGun/CMSSW_8_0_21/src/XiGun_GENSIM_IIDD_10k_events_decay_length_0cm_no_EvtGen_step2.root',
    # 'file:///user/jdeclerc/Analysis/dataSamples/ZeroBiasData/D07C27A2-5690-E711-844F-B083FED429D5.root'
    #'file:///user/jdeclerc/Analysis/ParticleGun/CMSSW_8_0_21/src/f2prime1525_GENSIM_IIDD_0point5to10GeV1k_MODIFIEDBRANCHINGRATIOS_MODPHI_MODBEAMSPOT_0cm_step2.root'
    )
@@ -116,7 +116,7 @@ process.p = cms.Path(
 
 # Output --> not used in the analyzer
 process.TFileService = cms.Service('TFileService',
-    fileName = cms.string('preFilterInfo.root'), 
+    fileName = cms.string('preFilterInfo_events_skimmed_2017_trialD.root'), 
 )
 
 #Keep edm output file --> used in the analyzer
@@ -126,6 +126,7 @@ process.out = cms.OutputModule("PoolOutputModule",
 
 #    'drop *',
 #    'keep *_InitialProducer_*_*',
+    'keep recoVertexs_offlinePrimaryVertices_*_*',
     'keep recoBeamSpot_offlineBeamSpot_*_*',
     'keep *_genParticles_*_HLT',
     'keep recoVertexCompositeCandidates_generalV0Candidates_*_*',
@@ -134,7 +135,8 @@ process.out = cms.OutputModule("PoolOutputModule",
     'keep recoVertexCompositePtrCandidates_sMassFilter_sVertexCompositePtrCandidate_*',
     'keep *_*_*_SEXAQ'
   ),
-  fileName = cms.untracked.string("events_skimmed_Xi1820.root"),
+#  fileName = cms.untracked.string("events_skimmed_2017_trialC.root"),
+  fileName = cms.untracked.string("events_skimmed_2017_trialD.root"),
   SelectEvents = cms.untracked.PSet(
     SelectEvents = cms.vstring('p')
   )
