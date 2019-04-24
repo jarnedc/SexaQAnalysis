3e66b02097ef0226e99d47738e70e96ad540cdec
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2019-04-24 22:09:48 +0200

    adding yet another analyzer

---
+++ AnalyzerAllSteps/python/AnalyzerAllSteps_cfi.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+AnalyzerAllSteps = cms.EDAnalyzer('AnalyzerAllSteps',
+    isData = cms.untracked.bool(True),
+    beamspot = cms.InputTag("offlineBeamSpot"),
+    offlinePV = cms.InputTag("offlinePrimaryVertices","","RECO"),
+    genCollection_GEN =  cms.InputTag("genParticles","","GEN"),
+    genCollection_SIM_GEANT =  cms.InputTag("genParticlesPlusGEANT","","SIM"),
+    generalTracksCollection =  cms.InputTag("generalTracks","","RECO"),
+    sexaqCandidates = cms.InputTag("lambdaKshortVertexFilter", "sParticles","SEXAQ"),
+    V0KsCollection = cms.InputTag("generalV0Candidates","Kshort","RECO"),
+    V0LCollection = cms.InputTag("generalV0Candidates","Lambda","RECO")
+)
